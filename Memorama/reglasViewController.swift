020988064c36c361f48acc42c583c0a9123b538b
RaulHMV <23170067@uttcampus.edu.mx>
2025-12-05 18:46:26 -0600

    First Commit: Desing w/o Logic

---
+++ Memorama/reglasViewController.swift
@@ -0,0 +1,29 @@
+//
+//  reglasViewController.swift
+//  Memorama
+//
+//  Created by Victor on 12/5/25.
+//
+
+import UIKit
+
+class reglasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
